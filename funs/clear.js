633e03cd853a067b7c9199b8a4802aeb8214f07b
Slasherss <jszumotalski@gmail.com>
2019-04-05 21:55:32 +0200

    Added clear command

---
+++ funs/clear.js
@@ -0,0 +1,23 @@
+exports.do() = function() => {
+  if (msg.content == "!clear") {
+    if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+      message.channel.sendMessage("Nie masz uprawnień do zarządzania wiadomościami.");
+      console.log("Użytkownik nie mam uprawnień");
+      return;
+    } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
+      message.channel.sendMessage("Nie mam uprawniń do zarządzania wiadomościami.");
+      console.log("Nie mam uprawnień do zarządzania wiadomościami.");
+      return;
+    }
+
+    if (message.channel.type == 'text') {
+      message.channel.fetchMessages()
+      .then(messages => {
+        message.channel.bulkDelete(messages);
+        messagesDeleted = messages.array().length;
+      })
+      .catch(err => {
+        console.log('Error while doing Bulk Delete');
+        console.log(err);
+      });
+}}}
---
+++ index.js
@@ -0,0 +1,20 @@
+global.Discord = require('discord.js');
+global.client = new Discord.Client();
+global.fs = require('fs');
+global.config = require('./config.json');
+
+client.on("message", message => {
+  global.message = message
+  if(message.author.bot) return;
+  fs.readdir('./funs/', (err, files) => {
+    files.forEach(file => {
+      loadcommand(file)
+    })
+  })
+
+  function loadcommand(file) {
+    require('./funs/' + file).do(msg)
+  }
+})
+
+client.login(config.token);
