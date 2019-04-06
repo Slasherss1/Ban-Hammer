59e589aa0937f69e6c5ccfe30d9f56178f4625e6
Slasherss <jszumotalski@gmail.com>
2019-04-06 23:00:47 +0200

    Added Kick command

--- funs/kick.js
+++ funs/kick.js
@@ -1,17 +1,17 @@
 exports.do = () => {
-  if (message.content.startsWith(`!ban`)) {
+  if (message.content.startsWith(`!kick`)) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
       if (member) {
-        member.ban().then(() => {
-          message.reply(`Zbanowano <@${user.id}>`)
+        member.kick().then(() => {
+          message.reply(`Wyrzucono <@${user.id}>`)
         })
       } else {
         message.reply('Ten urzytkownik nie jest na serwerze.');
       }
     } else {
-      message.reply('Nie powiedziałeś kogo zbanować!');
+      message.reply('Nie powiedziałeś kogo wyrzucić!');
     }
   }
 }
